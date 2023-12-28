972f5bf3528cc860e0ea48099d8014883b475f3b
kirinokirino <kirinokirin2501@gmail.com>
2023-12-28 20:42:51 +0300

    Add unused/not found font warnings, strip the csv fields

--- webdriver.py
+++ webdriver.py
@@ -10,12 +10,15 @@ def by_id(driver, id_):
 
 # font.link lable.font lable.text
 assets = {"lables": [], "fonts": []}
+font_names = set()
+lable_font_names = set()
 with open('assets.csv') as csvfile:
     print("reading assets.csv:")
     reader = csv.reader(csvfile)
     next(reader, None)
     for lable in reader:
-        new_lable = {"text": lable[0], "font": lable[1]}
+        new_lable = {"text": lable[0].strip(), "font": lable[1].strip()}
+        lable_font_names.add(lable[1].strip())
         assets["lables"].append(new_lable)
 print(assets["lables"])
 
@@ -24,9 +27,16 @@ with open('fonts.csv') as csvfile:
     reader = csv.reader(csvfile)
     next(reader, None)
     for font in reader:
-        new_font = {"font": font[0], "link": font[1]}
+        new_font = {"font": font[0].strip(), "link": font[1].strip()}
+        font_names.add(font[0].strip())
         assets["fonts"].append(new_font)
 print(assets["fonts"])
+
+if len(lable_font_names - font_names) > 0:
+    print("WARNING: Fonts used in lables, that are not found in fonts table: " + str(lable_font_names - font_names))
+
+if len(font_names - lable_font_names) > 0:
+    print("WARNING: Unused fonts: " + str(font_names - lable_font_names))
 
 print("loading jinja2 environment")
 jinja = jinja2.Environment(loader=jinja2.FileSystemLoader("./"))
